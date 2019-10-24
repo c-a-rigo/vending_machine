c63c3696ae52f6b6090326f81cff632036bff7bb
c-a-rigo <c-a-rigo@users.noreply.github.com>
2019-10-23 21:42:56 -0300

    Create InterfacePc.h

---
+++ InterfacePc.h
@@ -0,0 +1,15 @@
+#ifndef INTERFACEPC_H
+#define INTERFACEPC_H
+#include "Interface.h"
+
+class InterfacePc : public Interface
+{
+protected:
+    string entrada1;
+
+public:
+    string get_entrada();
+    void processa(string);
+    void report(string refri, int data, int hora, int preco);
+};
+#endif

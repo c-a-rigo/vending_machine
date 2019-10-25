d40e66cf578251e06293b14208b417fcaf7b3b64
c-a-rigo <c-a-rigo@users.noreply.github.com>
2019-10-25 08:42:49 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,18 +2,29 @@
 #include <string>
 #include <stdlib.h>
 
+#define ATL
+
+extern int repo = 0;
+extern int data = 12121999;
+extern int hora = 1900;
+
 #include "Machine.cpp"
+#include "BancoDados.cpp"
 
 int main()
 {
 
-    int data = 12121999;
-    int hora = 1900;
-
     Machine Venda;
+    BancoDados Dados;
 
     while (1)
     {
-        Venda.venda(data, hora);
+        if (Venda.venda())
+        {
+            //cout << data;
+            Dados.reporta();
+        }
+
+        cout << repo;
     }
 }

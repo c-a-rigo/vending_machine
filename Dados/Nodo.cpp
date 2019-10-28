bf51bffc031817bc89c9375911f4e0b346ca3b73
c-a-rigo <c-a-rigo@users.noreply.github.com>
2019-10-28 09:09:00 -0300

    Create Nodo.cpp

---
+++ Dados/Nodo.cpp
@@ -0,0 +1,60 @@
+#include "Nodo.h"
+
+Nodo::Nodo(string &r, int &t, int &o, int &k, Nodo *nxt)
+{
+    this->refri = r;
+    this->data = t;
+    this->hora = o;
+    this->preco = k;
+    next = nxt;
+}
+
+string Nodo::get_refri()
+{
+    return refri;
+}
+
+int Nodo::get_data()
+{
+    return data;
+}
+
+int Nodo::get_hora()
+{
+    return hora;
+}
+
+int Nodo::get_preco()
+{
+    return preco;
+}
+
+Nodo *Nodo::get_next()
+{
+    return next;
+}
+
+void Nodo::set_refri(string refri)
+{
+    this->refri = refri;
+}
+
+void Nodo::set_data(int data)
+{
+    this->data = data;
+}
+
+void Nodo::set_hora(int hora)
+{
+    this->hora = hora;
+}
+
+void Nodo::set_preco(int preco)
+{
+    this->preco = preco;
+}
+
+void Nodo::set_next(Nodo *nxt)
+{
+    next = nxt;
+}

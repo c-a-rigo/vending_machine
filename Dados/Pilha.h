37529e121feff7e1a4c87aa700006eaba3e37377
c-a-rigo <c-a-rigo@users.noreply.github.com>
2019-10-28 09:08:51 -0300

    Create Pilha.h

---
+++ Dados/Pilha.h
@@ -0,0 +1,19 @@
+#ifndef PILHA_H
+#define PILHA_H
+#include <iostream>
+using namespace std;
+
+class Pilha
+{
+private:
+    Nodo *topo, *aux;
+
+public:
+    Pilha();
+    ~Pilha();
+    void push(string refri, int preco);
+    void pop(string &r, int &t, int &o, int &k);
+    int pilha_vazia();
+};
+
+#endif

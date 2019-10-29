fd8be4bec173c0cb8906b78f36bc4598996d88eb
c-a-rigo <c-a-rigo@users.noreply.github.com>
2019-10-29 10:38:24 -0300

    Update Interface.h

--- Interface/Interface.h
+++ Interface/Interface.h
@@ -9,8 +9,8 @@ class Interface
 public:
     virtual string get_entrada() = 0;
     virtual void processa(string) = 0;
-    virtual void report(string refri, int dat, int hora, int preco) = 0;
-    virtual void report_final(string x, int y, string f) = 0;
+    virtual void report(string refri, int data, int hora, int preco) = 0;
+    virtual void report_final(string x, int y, string f, int z) = 0;
 };
 
 #endif

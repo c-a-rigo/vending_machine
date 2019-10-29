27f846b6a456812ca089610511cc3d4b4854767f
c-a-rigo <c-a-rigo@users.noreply.github.com>
2019-10-29 10:38:50 -0300

    Update Pilha.cpp

--- Dados/Pilha.cpp
+++ Dados/Pilha.cpp
@@ -1,49 +1,73 @@
 #include "Pilha.h"
 #include "../common.h"
 
-Pilha::Pilha()
+Pilha::Pilha() //controi a Pilha.
 {
-    topo = 0;
+    topo = 0; //inicia com zero nodos.
 }
 
-Pilha::~Pilha()
+Pilha::~Pilha() //destroi a Pilha.
 {
-    while (topo)
+    while (topo) //enquanto houver um nodo.
     {
-        aux = topo->get_next();
-        delete topo;
-        topo = aux;
+        aux = topo->get_next(); //salva o endereço para o proximo nodo.
+        delete topo;            //deleta o nodo atual.
+        topo = aux;             //atual = proximo.
     }
+
+    //zera todos os ponteiros.
     topo = 0;
     aux = 0;
 }
 
 void Pilha::push(string refri, int preco)
 {
-    aux = new Nodo(refri, g_data, g_hora, preco, topo);
-    //aux->set_next(topo);
+    /*  Funçao para inserir valores em um novo nodo da Pilha. 
 
-    topo = aux;
+    Argumentos:
+    refri: O refrigerante vendido.
+    g_data: A data da venda.
+    g_hora: A hora da venda.
+    precp: O preco do refrigerante.
+    
+   */
+
+    aux = new Nodo(refri, g_data, g_hora, preco, topo); //aux recebe o endereco do novo nodo criado com o comando new.
+    topo = aux;                                         //topo passa a apontar para o novo nodo.
 }
 
 void Pilha::pop(string &r, int &t, int &o, int &k)
 {
+    /*  Funçao para ler os valores no nodo do topo da pilha.
+
+    Argumentos:
+    r: O refrigerante vendido.
+    t: A data da venda.
+    o: A hora da venda.
+    k: O preco do refrigerante.
+
+   */
 
     aux = topo;
 
-    if (aux)
+    if (aux) //se houver um nodo.
     {
-        topo = aux->get_next();
+        topo = aux->get_next(); //faz topo apontar para o proximo nodo na pilha.
+
+        //pega toos valores no nodo.
         r = aux->get_refri();
         t = aux->get_data();
         o = aux->get_hora();
         k = aux->get_preco();
-        delete (aux);
+
+        delete (aux); //deleta o nodo.
     }
 }
 
 int Pilha::pilha_vazia()
 {
+    /* Função para verificar se a pilha esta vazia. */
+
     if (topo)
         return 0;
     else

c10a0eb710f3249a2f4d573145ecb69d81354dd6
c-a-rigo <c-a-rigo@users.noreply.github.com>
2019-10-29 10:39:01 -0300

    Update Lista.cpp

--- Dados/Lista.cpp
+++ Dados/Lista.cpp
@@ -1,18 +1,20 @@
 #include "Lista.h"
 
-Lista::Lista()
+Lista::Lista() //constroi
 {
-    top = 0;
+    top = 0; //inicia sem nodos
 }
 
-Lista::~Lista()
+Lista::~Lista() //destroi
 {
-    while (top)
+    while (top) //enquanto houverem nodos na lista
     {
-        auxiliar = top->get_next();
-        delete top;
-        top = auxiliar;
+        auxiliar = top->get_next(); //salva o endereço do próximo nodo
+        delete top;                 //deleta o nodo atual
+        top = auxiliar;             //atual = proximo
     }
+
+    //zera todos os ponteiros
     top = 0;
     auxiliar = 0;
     mid = 0;
@@ -20,29 +22,49 @@ Lista::~Lista()
 
 void Lista::push(string &r, int &t, int &o, int &k)
 {
-    auxiliar = new Nodo(r, t, o, k, top);
-    //aux->set_next(topo);
-    top = auxiliar;
-    mid = top;
+    /*  Funçao para inserir valores em um novo nodo da lista. 
+
+    Argumentos:
+    r: O refrigerante vendido.
+    t: A data da venda.
+    o: A hora da venda.
+    k: O preco do refrigerante.
+    
+   */
+
+    auxiliar = new Nodo(r, t, o, k, top); //auxiliar recebe endereço do proximo nodo criado com o comando new.
+    top = auxiliar;                       //topo aponta para o mais novo nodo.
+    mid = top;                            //mid aponta para o mais novo nodo.
 }
 
 Nodo *Lista::get_mid()
 {
-    return mid;
+    return mid; //retorna valor do ponteiro mid.
 }
 
 Nodo *Lista::get_mid_next()
 {
-    return mid->get_next();
+    return mid->get_next(); //retorna endereco do proximo nodo para o qual o mid aponta
 }
 
 void Lista::set_mid(Nodo *nxt)
 {
-    mid = nxt;
+    mid = nxt; //seta um valoar para o ponteiro mid.
 }
 
 void Lista::ler(string &r, int &t, int &o, int &k, Nodo *n)
 {
+    /*  Funçao para listar os valores salvos no nó n da lista. 
+
+    Argumentos:
+    r: O refrigerante vendido.
+    t: A data da venda.
+    o: A hora da venda.
+    k: O preco do refrigerante.
+    n: Ponteiro para um nodo.
+
+   */
+
     if (n)
     {
         r = n->get_refri();

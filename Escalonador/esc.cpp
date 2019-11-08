d4edb97226bc5a486c423f124a06f8f4423069ed
c-a-rigo <c-a-rigo@users.noreply.github.com>
2019-11-08 15:11:40 -0300

    Update esc.cpp

--- Escalonador/esc.cpp
+++ Escalonador/esc.cpp
@@ -4,10 +4,19 @@
 
 #include "ListaEsc.cpp"
 
+#include "../Machine/Machine.cpp"
+#include "../Dados/BancoDados.cpp"
+
+void foo()
+{
+    cout << "I was here." << endl;
+}
+
 int main()
 {
-
+    Machine Venda;
     ListaEsc Listax;
 
-    //Listax.push()
+    Listax.push(1, 2, 3, 4, 5, &Venda.venda);
+    Listax.teste();
 }

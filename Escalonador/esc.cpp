fc26664c5d7df2bd1c8ebd9ace74452cfa4c6f11
c-a-rigo <c-a-rigo@users.noreply.github.com>
2019-11-11 19:04:53 -0300

    Update esc.cpp

--- Escalonador/esc.cpp
+++ Escalonador/esc.cpp
@@ -1,22 +1,42 @@
 #include <iostream>
 #include <string>
 #include <stdlib.h>
+#include <unistd.h>
 
-#include "ListaEsc.cpp"
+using namespace std;
 
-#include "../Machine/Machine.cpp"
-#include "../Dados/BancoDados.cpp"
+#include "Escalonador.cpp"
 
 void foo()
 {
-    cout << "I was here." << endl;
+    cout << "     T2  P1";
+}
+
+void foo2()
+{
+    cout << "     T5  P3";
+}
+
+void foo3()
+{
+    cout << "     T10  P2";
 }
 
 int main()
 {
-    Machine Venda;
-    ListaEsc Listax;
+    Escalonador Escalonador;
 
-    Listax.push(1, 2, 3, 4, 5, &Venda.venda);
-    Listax.teste();
+    Escalonador.escalona(2, 1, 0, 1, 0, &foo); //periodo, prioridade, pronta, habilitada, bloqueada, funçao
+    Escalonador.escalona(5, 3, 0, 1, 0, &foo2);
+    Escalonador.escalona(10, 2, 0, 1, 0, &foo3);
+
+    int b = 0;
+    while (b < 1000)
+    {
+        Escalonador.executa();
+        b++;
+        //usleep(1000000);
+        if (b == 100)
+            Escalonador.bloqueia(&foo);
+    }
 }

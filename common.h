8a33bc72d7ce4dea709e2dd08481194b429a9539
c-a-rigo <c-a-rigo@users.noreply.github.com>
2019-11-24 17:14:24 -0300

    Update common.h

--- common.h
+++ common.h
@@ -1,10 +1,10 @@
 #ifndef COMMON_H_
 #define COMMON_H_
 
-//extern int venda = 0;
-//extern int repo = 0;
-extern int g_data = 12121999;
-extern int g_hora = 1100;
+extern int g_data = 0;
+extern int g_hora = 0;
 extern int repo = 0;
+extern int repo1 = 0;
+extern int th = 0;
 
 #endif

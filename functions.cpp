3160d92fdccc99805e2f7c8b67bb491e6a57e25c
c-a-rigo <c-a-rigo@users.noreply.github.com>
2019-11-24 17:14:14 -0300

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -2,7 +2,6 @@
 #include <string>
 #include <stdlib.h>
 #include <unistd.h>
-#include <pthread.h>
 
 #include "Machine/Machine.cpp"
 #include "Dados/BancoDados.cpp"
@@ -10,7 +9,14 @@
 
 Machine Venda;
 BancoDados Dados;
-ClockCalendar Calendario(20, 11, 2019, 6, 07, 55, 1);
+ClockCalendar Calendario(2, 10, 2019, 6, 07, 55, 1);
+//                                    ^
+//                                    |
+//                     mes, dia, ano, hora, min, seg, ispm
+
+#ifdef PC
+
+#include <pthread.h>
 
 void *linha(void *)
 {
@@ -28,22 +34,54 @@ void *linha(void *)
         Dados.registra(sell, money);
         venda = 0;
     }
+    th = 0;
 }
+
+void vender()
+{
+    if (!th) //se não há uma thread ativa então cria
+    {
+        th = 1;
+        pthread_t t1;
+        pthread_create(&t1, NULL, &linha, NULL);
+    }
+}
+
+#endif
+
+#ifdef ATL
 
 void sell()
 {
-    pthread_t t1;
-    //pthread_t *outsideMain = malloc(sizeof(pthread_t));
+    string sell;
+    int money;
 
-    pthread_create(&t1, NULL, &linha, NULL);
+    int venda = 0;
+
+    Venda.venda(sell, money, repo, venda);
+
+    if (venda)
+    {
+        Dados.registra(sell, money);
+        venda = 0;
+    }
 }
 
-void repor()
+#endif
+void reportar()
 {
+
+    if (repo1)
+    {
+        Dados.read_all_list(Venda);
+
+        repo1 = 0;
+    }
+
     if (repo)
     {
         Dados.reporta(Venda);
-        //Dados.read_all_list(Venda);
+
         repo = 0;
     }
 
@@ -53,85 +91,117 @@ void repor()
 void atualiza_tempo()
 {
 
-    string aro = "", ata = "";
+    string proc_hora = "", proc_data = "";
 
     int m, d, y;
     int hr, mi, sec, is_pm;
 
-    usleep(1000000);
+    for (int l = 0; l < 610; l++)
+    {
+        ++Calendario;
+    }
 
     ++Calendario;
     Calendario.readCalendar(m, d, y);
     Calendario.readClock(hr, sec, mi, is_pm);
 
-    std::string str1 = std::to_string(m);
-    std::string str2 = std::to_string(d);
-    std::string str3 = std::to_string(y);
-    std::string str4 = std::to_string(hr);
-    std::string str6 = std::to_string(mi);
+    std::string s_mes = std::to_string(m);
+    std::string s_dia = std::to_string(d);
+    std::string s_ano = std::to_string(y);
+    std::string s_hora = std::to_string(hr);
+    std::string s_min = std::to_string(mi);
+    std::string s_seg = std::to_string(sec);
 
     if (is_pm == 0)
     {
-        if (d < 10)
-            ata = "0" + str2 + str1 + str3;
-        if (m < 10)
-            ata = str2 + "0" + str1 + str3;
-        if (d < 10 && m < 10)
-            ata = "0" + str2 + "0" + str1 + str3;
-        else
-        {
-            ata = str2 + str1 + str3;
-        }
+        proc_data = s_dia + s_mes + s_ano;
 
-        int i = atoi(ata.c_str());
+        if (d < 10)
+            proc_data = "0" + s_dia + s_mes + s_ano;
+        if (m < 10)
+            proc_data = s_dia + "0" + s_mes + s_ano;
+        if (d < 10 && m < 10)
+            proc_data = "0" + s_dia + "0" + s_mes + s_ano;
+
+        int i = atoi(proc_data.c_str());
         g_data = i;
 
+        proc_hora = s_hora + s_min;
         if (mi < 10)
-            aro = str4 + "0" + str6;
-        else
-        {
-            aro = str4 + str6;
-        }
+            proc_hora = s_hora + "0" + s_min;
 
-        i = atoi(aro.c_str());
+        i = atoi(proc_hora.c_str());
         g_hora = i;
     }
     else
     {
+        proc_data = s_dia + s_mes + s_ano;
         if (d < 10)
-            ata = "0" + str2 + str1 + str3;
+            proc_data = "0" + s_dia + s_mes + s_ano;
         if (m < 10)
-            ata = str2 + "0" + str1 + str3;
+            proc_data = s_dia + "0" + s_mes + s_ano;
         if (d < 10 && m < 10)
-            ata = "0" + str2 + "0" + str1 + str3;
-        else
-        {
-            ata = str2 + str1 + str3;
-        }
+            proc_data = "0" + s_dia + "0" + s_mes + s_ano;
 
-        int i = atoi(ata.c_str());
+        int i = atoi(proc_data.c_str());
 
         g_data = i;
 
-        std::string str4 = std::to_string(hr);
+        hr = hr + 12;
+        std::string s_hora = std::to_string(hr);
 
+        proc_hora = s_hora + s_min;
         if (mi < 10)
-            aro = str4 + "0" + str6;
-        else
-        {
-            aro = str4 + str6;
-        }
-        i = atoi(aro.c_str());
+            proc_hora = s_hora + "0" + s_min;
+
+        i = atoi(proc_hora.c_str());
         g_hora = i;
     }
 
 #ifdef HR
 
-    std::string str5 = std::to_string(sec);
+    if (is_pm == 0)
+    {
+        proc_data = s_dia + "/" + s_mes + "/" + s_ano;
 
-    string aab = str1 + "/" + str2 + "/" + str3 + " " + str4 + ":" + str6 + ":" + str5;
+        if (d < 10)
+            proc_data = "0" + s_dia + "/" + s_mes + "/" + s_ano;
+        if (m < 10)
+            proc_data = s_dia + "/" + "0" + s_mes + "/" + s_ano;
+        if (d < 10 && m < 10)
+            proc_data = "0" + s_dia + "/" + "0" + s_mes + "/" + s_ano;
 
-    cout << aab << endl;
+        proc_hora = s_hora + ":" + s_min + ":" + s_seg + " a.m.";
+        if (mi < 10)
+            proc_hora = s_hora + ":" + "0" + s_min + ":" + s_seg + " a.m.";
+        if (sec < 10)
+            proc_hora = s_hora + ":" + s_min + ":" + "0" + s_seg + " a.m.";
+        if (mi < 10 && sec < 10)
+            proc_hora = s_hora + ":" + "0" + s_min + ":" + "0" + s_seg + " a.m.";
+    }
+    else
+    {
+        hr = hr - 12;
+        proc_data = s_dia + "/" + s_mes + "/" + s_ano;
+        if (d < 10)
+            proc_data = "0" + s_dia + "/" + s_mes + "/" + s_ano;
+        if (m < 10)
+            proc_data = s_dia + "/" + "0" + s_mes + "/" + s_ano;
+        if (d < 10 && m < 10)
+            proc_data = "0" + s_dia + "/" + "0" + s_mes + "/" + s_ano;
+
+        proc_hora = s_hora + ":" + s_min + ":" + s_seg + " p.m.";
+        if (mi < 10)
+            proc_hora = s_hora + ":" + "0" + s_min + ":" + s_seg + " p.m.";
+        if (sec < 10)
+            proc_hora = s_hora + ":" + s_min + ":" + "0" + s_seg + " p.m.";
+        if (mi < 10 && sec < 10)
+            proc_hora = s_hora + ":" + "0" + s_min + ":" + "0" + s_seg + " p.m.";
+    }
+
+    //string aab = s_mes + "/" + s_dia + "/" + s_ano + " " + s_hora + ":" + s_min + ":" + s_seg;
+
+    cout << proc_data << " " << proc_hora << endl;
 
 #endif
 }
